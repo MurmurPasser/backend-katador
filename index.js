88ad3f655727d0b5c8415a7b1c4b4bc447c9b091
MurmurPasser <j.agual787@gmail.com>
2025-04-25 15:34:08 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const authRoutes = require('./routes/auth');
-require('dotenv').config();
 
+const app = express();
+app.use(cors());
 app.use(express.json());
+
 app.use('/api', authRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/register', (req, res) => {
-  // lógica de registro
-  res.send('Usuario registrado');
+    res.send("Usuario registrado");
 });
 
 router.post('/login', (req, res) => {
-  // lógica de login
-  res.send('Usuario logueado');
+    res.send("Usuario logueado");
 });
 
 module.exports = router;
